8f2392be722783341f2ffe7156a39951a2806ed3
HasinaBegumShaik <hasinabegum.shaik@gmail.com>
2022-02-28 17:41:09 +0530

    Update SortRectangleTest.cs

--- SortRectangleTest/SortRectangleTest.cs
+++ SortRectangleTest/SortRectangleTest.cs
@@ -1,18 +1,48 @@
 using NUnit.Framework;
+using SortRectangles.Model;
+using System.Collections.Generic;
 
-namespace Tests
+namespace SortLogicTest
 {
-    public class Tests
+    public class SortLogicTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+
 
         [Test]
-        public void Test1()
+        public void SortDescnding()
         {
-            Assert.Pass();
+            var sorting = new SortRectangleLogic();
+
+            var Rectanglelist = new List<Rectangle>
+            {
+           new Rectangle(){length=3,width=4},
+            new Rectangle(){length=5,width=5},
+            new Rectangle(){length=4,width=4}
+            };
+
+            List<Rectangle> sortedlist = sorting.GetSortedRectngles(Rectanglelist);
+
+            Assert.Equals(5, sortedlist[0].length);
+        }
+
+
+
+        [Test]
+        public void NegativeValuesTest()
+        {
+            var sorting = new SortRectangleLogic();
+
+            var Rectanglelist = new List<Rectangle>
+            {
+           new Rectangle(){length=-3,width=4},
+            new Rectangle(){length=-5,width=5},
+            new Rectangle(){length=-4,width=4}
+            };
+
+            List<Rectangle> sortedlist = sorting.GetSortedRectngles(Rectanglelist);
+
+            Assert.Warn("Please Enter Valid Length and width");
+
         }
     }
 }
